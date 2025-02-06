6a593155ceb203a2d5c8c255072841b593965f04
kforsl <forslund95@live.com>
2025-02-06 22:30:00 +0100

    change artistCard img url

--- client/src/Pages/Home.elm
+++ client/src/Pages/Home.elm
@@ -218,6 +218,6 @@ viewArtistCard artist =
             ]
             [ Html.text artist.name ]
         , Html.img
-            [ HA.src "https://images.unsplash.com/photo-1587116288118-56068e06763d?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" ]
+            [ HA.src artist.profileImgUrl ]
             []
         ]
