f42b9b827782fdd7ca835e89daeb04774cf67b15
kforsl <forslund95@live.com>
2025-02-21 23:48:14 +0100

    layout artwork page

--- client/src/Pages/Artwork.elm
+++ client/src/Pages/Artwork.elm
@@ -4,7 +4,6 @@ import Api.Artist exposing (Artist)
 import Api.Artwork exposing (Artwork)
 import Html exposing (Html)
 import Html.Attributes as HA
-import Html.Extra
 
 
 type alias Model =
@@ -32,28 +31,24 @@ update msg model =
 view : Model -> String -> Html Msg
 view model id =
     let
-        ( matchingArtwork, otherArtwork ) =
-            List.partition (\x -> x.id == id) model.artworkData
+        matchingArtwork =
+            List.filter (\x -> x.id == id) model.artworkData
     in
     case List.head matchingArtwork of
         Just artwork ->
-            let
-                artistId =
-                    artwork.artist.id
-            in
-            Html.main_ [ HA.class "max-w-svw min-h-full grid auto-rows-max place-content-between grid-cols-1 " ]
+            Html.main_ [ HA.class "max-w-svw " ]
                 [ viewArtwork artwork
                 , viewArtworkInformation artwork
-                -- , viewArtistInformation artwork.artist
-                , viewArtistOtherArtwork otherArtwork artistId
+                , viewArtistInformation artwork.artist
                 ]
 
         Nothing ->
             Html.text "Error Not Found"
 
+
 viewArtwork : Artwork -> Html Msg
 viewArtwork artwork =
-    Html.figure [ HA.class "bg-bgDark h-full relative z-0 py-16" ]
+    Html.figure [ HA.class "bg-bgDark h-5/6 z-0 py-16 bg-text" ]
         [ Html.img
             [ HA.src artwork.imageUrl, HA.class "h-full mx-auto" ]
             []
@@ -64,94 +59,53 @@ viewArtworkInformation : Artwork -> Html Msg
 viewArtworkInformation artwork =
     Html.section [ HA.class "bg-bgDark relative z-0" ]
         [ Html.div
-            [ HA.class "max-w-maxWidth h-full m-auto p-8" ]
+            [ HA.class "max-w-maxWidth m-auto p-8 grid gap-4 grid-cols-4 justify-items-center" ]
             [ Html.h2
-                [ HA.class "text-5xl font-title text-primary mb-4" ]
+                [ HA.class "text-4xl font-title text-primary mb-4 col-span-full" ]
                 [ Html.text artwork.title ]
             , Html.h3
-                [ HA.class "text-2xl font-title text-primary mb-4" ]
+                [ HA.class "text-3xl font-title text-primary mb-4 col-span-full" ]
                 [ Html.text ("Created by: " ++ artwork.artist.name) ]
             , Html.p
-                [ HA.class "font-bread text-base text-textLight mb-8" ]
+                [ HA.class "font-bread text-base text-textLight mb-8 col-span-2" ]
                 [ Html.text artwork.description ]
-            , Html.section [ HA.class "flex justify-between" ]
-                [ Html.h3
-                    [ HA.class "text-3xl font-title text-primary mb-4" ]
-                    [ Html.text "Styles" ]
-                , Html.ul
-                    []
-                    (List.map viewListItem artwork.styles)
-                , Html.h3
-                    [ HA.class "text-3xl font-title text-primary mb-4" ]
-                    [ Html.text "Mediums" ]
-                , Html.ul
-                    []
-                    (List.map viewListItem artwork.mediums)
-                ]
+            , viewList "Styles" artwork.styles
+            , viewList "Mediums" artwork.mediums
             ]
         ]
 
 
 viewArtistInformation : Artist -> Html Msg
 viewArtistInformation artist =
-    Html.section [ HA.class "bg-bgDark relative z-0" ]
+    Html.section [ HA.class "bg-bgDark relative z-0 flex justify-center" ]
         [ Html.div
             [ HA.class "max-w-maxWidth m-auto flex gap-8 py-24" ]
-            [ Html.img [ HA.src artist.profileImgUrl, HA.class "m-auto" ] []
-            , Html.section [ HA.class "flex-grow" ]
+            [ Html.section [ ]
                 [ Html.h2
-                    [ HA.class "text-5xl font-title text-primary mb-4" ]
-                    [ Html.text artist.name ]
+                    [ HA.class "text-3xl font-title text-primary mb-4" ]
+                    [ Html.text "About the Artist" ]
                 , Html.p
-                    [ HA.class "font-bread text-base text-textLight mb-8" ]
+                    [ HA.class "font-bread text-base text-textLight mb-8 max-w-[50ch]" ]
                     [ Html.text artist.aboutMe ]
-                , Html.section [ HA.class "flex justify-between" ]
-                    [ Html.h3
-                        [ HA.class "text-3xl font-title text-primary mb-4" ]
-                        [ Html.text "Styles" ]
-                    , Html.ul
-                        []
-                        (List.map viewListItem artist.styles)
-                    , Html.h3
-                        [ HA.class "text-3xl font-title text-primary mb-4" ]
-                        [ Html.text "Mediums" ]
-                    , Html.ul
-                        []
-                        (List.map viewListItem artist.mediums)
-                    ]
                 ]
+            , Html.img [ HA.src artist.profileImgUrl, HA.class "h-80" ] []
             ]
         ]
 
 
-viewListItem : String -> Html msg
-viewListItem x =
-    Html.li [ HA.class "text-textLight" ] [ Html.text x ]
-
-
-viewArtistOtherArtwork : List Artwork -> String -> Html Msg
-viewArtistOtherArtwork artworks artistId =
-    Html.section
-        [ HA.class " max-w-maxWidth m-auto py-24 flex flex-wrap gap-4" ]
-        (List.map
-            (\artwork ->
-                Html.Extra.viewIf (artwork.artist.id == artistId)
-                    (viewArtworkCard artwork)
-            )
-            artworks
+viewList : String -> List String -> Html Msg
+viewList label list =
+    Html.ul
+        [ HA.class "flex flex-col gap-2" ]
+        (Html.h4
+            [ HA.class "text-2xl mb-4 font-title font-semibold text-primary" ]
+            [ Html.text label ]
+            :: List.map viewListChip list
         )
 
 
-viewArtworkCard : Artwork -> Html msg
-viewArtworkCard artwork =
-    Html.article
-        [ HA.class "max-w-44 grid gap-0.5" ]
-        [ Html.a
-            [ HA.href ("/artwork/" ++ artwork.id)
-            , HA.class "font-title text-base overflow-hidden text-ellipsis text-nowrap underline underline-offset-2 cursor-pointer"
-            ]
-            [ Html.text artwork.title ]
-        , Html.img
-            [ HA.src artwork.imageUrl ]
-            []
-        ]
+viewListChip : String -> Html Msg
+viewListChip label =
+    Html.li
+        [ HA.class "py-2 px-4 rounded-full bg-bgLight bg-opacity-25 w-fit text-xs font-bread" ]
+        [ Html.text label ]
