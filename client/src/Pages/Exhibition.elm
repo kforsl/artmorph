a5fc8d5880012357a25b26f2933fc4805ffe1c22
kforsl <forslund95@live.com>
2025-03-01 17:32:21 +0100

    responsiv design exhibition page

--- client/src/Pages/Exhibition.elm
+++ client/src/Pages/Exhibition.elm
@@ -51,18 +51,18 @@ view model id =
 
 viewHero : Exhibition -> Html Msg
 viewHero exhibition =
-    Html.section [ HA.class "bg-bgDark relative z-0 bg-text" ]
+    Html.section [ HA.class "bg-bgDark relative z-0 bg-text h-fit" ]
         [ Html.div
-            [ HA.class "max-w-maxWidth m-auto flex gap-8 py-24" ]
-            [ Html.img [ HA.src exhibition.thumbnailUrl ] []
-            , Html.section [ HA.class "flex-grow p-4" ]
+            [ HA.class "max-w-maxWidth m-auto grid md:grid-cols-3 gap-8 grid-cols-2 sm:py-24 px-4 py-8" ]
+            [ Html.img [ HA.src exhibition.thumbnailUrl, HA.class "md:col-span-1 col-span-full md:row-span-2 mx-auto" ] []
+            , Html.section [ HA.class "p-4 md:col-span-2 col-span-full" ]
                 [ Html.h2
-                    [ HA.class "text-5xl font-title text-primary mb-4" ]
+                    [ HA.class "font-title text-primary mb-4 lg:text-5xl sm:text-3xl text-xl" ]
                     [ Html.text exhibition.title ]
                 , Html.p
-                    [ HA.class "  text-base text-textLight mb-8" ]
+                    [ HA.class "sm:text-base text-sm sm:mb-8 mb-4 text-textLight" ]
                     [ Html.text exhibition.description ]
-                , Html.section [ HA.class "flex justify-evenly" ]
+                , Html.section [ HA.class "grid gap-4 p-4 md:col-span-2 col-span-full sm:grid-cols-2" ]
                     [ viewList "Styles" exhibition.styles
                     , viewList "Mediums" exhibition.mediums
                     ]
@@ -92,14 +92,14 @@ viewListChip label =
 viewArtworks : List ExhibitionArtwork -> Html Msg
 viewArtworks artworks =
     Html.section
-        [ HA.class " max-w-maxWidth m-auto py-24 grid grid-cols-2 gap-8" ]
+        [ HA.class "max-w-maxWidth m-auto py-24 grid md:grid-cols-2 gap-8" ]
         (List.indexedMap viewArtworkCard artworks)
 
 
 viewArtworkCard : Int -> ExhibitionArtwork -> Html Msg
 viewArtworkCard x artwork =
     Html.article
-        [ HA.class ("grid grid-cols-2 gap-4 relative  p-2 group hover:opacity-80 focus-within:opacity-80 row-span-2 row-start-" ++ String.fromInt (x + 1))
+        [ HA.class ("grid grid-cols-2 gap-4 relative p-2 group hover:opacity-80 focus-within:opacity-80 row-span-2 md:row-start-" ++ String.fromInt (x + 1))
         ]
         [ Html.section
             [ HA.href ("/artwork/" ++ artwork.id)
@@ -109,18 +109,19 @@ viewArtworkCard x artwork =
                 ]
             ]
             [ Html.h2
-                [ HA.class "text-xl font-title font-semibold" ]
+                [ HA.class "md:text-xl text-lg font-title font-semibold" ]
                 [ Html.text artwork.title ]
             , Html.p
-                [ HA.class "  text-sm h-56 overflow-clip" ]
+                [ HA.class "md:text-sm text-xs h-56 overflow-clip" ]
                 [ Html.text artwork.description ]
             , Html.h3
-                [ HA.class "text-lg font-title underline text-center group-hover:text-primary group-focus-within:text-primary"
+                [ HA.class "md:text-lg md:text-base font-title underline text-center group-hover:text-primary group-focus-within:text-primary"
                 ]
                 [ Html.text "view Artwork" ]
             ]
         , Html.img
             [ HA.src artwork.imageUrl
+            , HA.class "place-self-center"
             ]
             []
         , Html.a
@@ -140,14 +141,14 @@ viewCreatedBy artworks artists =
                 artists
     in
     Html.article
-        [ HA.class "bg-bgDark bg-text" ]
+        [ HA.class "bg-bgDark bg-text px-4 py-8" ]
         [ Html.section
-            [ HA.class "max-w-maxWidth flex gap-12 m-auto py-24 border-b-2 border-bgDark" ]
+            [ HA.class "max-w-maxWidth flex flex-wrap justify-center gap-x-12 gap-y-4 m-auto md:py-24 border-b-2 border-bgDark" ]
             [ Html.h2
-                [ HA.class "font-title text-textLight text-4xl mb-8 col-span-full" ]
+                [ HA.class "font-title text-textLight sm:text-3xl text-xl mb-8 col-span-full" ]
                 [ Html.text "Created by: " ]
             , Html.ul
-                [ HA.class "flex overflow-hidden justify-center gap-12" ]
+                [ HA.class "flex flex-wrap overflow-hidden justify-center md:gap-12 gap-8" ]
                 (List.map viewArtistCard exhibitionArtists)
             ]
         ]
@@ -177,15 +178,15 @@ viewArtistCard artist =
 viewOtherExhibitions : List Exhibition -> Html Msg
 viewOtherExhibitions exhibitions =
     Html.article
-        [ HA.class "max-w-maxWidth m-auto py-24" ]
+        [ HA.class "max-w-maxWidth m-auto py-24 w-full" ]
         [ Html.section
-            [ HA.class "flex justify-between" ]
+            [ HA.class "flex flex-wrap justify-between" ]
             [ Html.h2
-                [ HA.class "font-title text-3xl mb-4 text-textDark col-span-full" ]
-                [ Html.text "Other Exhibitions" ]
+                [ HA.class "font-title md:text-3xl text-2xl mb-4 text-textDark col-span-full" ]
+                [ Html.text "Featured Exhibitions" ]
             , Html.a
                 [ HA.href "/exhibitions"
-                , HA.class "place-self-center text-nowrap text-base h-fit py-2.5 px-4 bg-primary rounded-2xl font-bold hover:opacity-80 focus-within:opacity-80"
+                , HA.class "place-self-center text-nowrap sm:text-base text-sm h-fit py-2.5 px-4 bg-primary   rounded-2xl font-bold hover:opacity-80 focus-within:opacity-80"
                 ]
                 [ Html.text "Checkout all our exhibitions" ]
             ]
